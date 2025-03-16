3eadf71582ae55a90041dbdbc8774b106abd70dd
william <williamburbatt@yahoo.com>
2025-03-16 14:54:34 -0500

    test

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder="front-end/code")
 
 @app.route("/")
 def home():
-    return "Hello World!"
+    return render_template("index.html")  # Change this if your main file is different
+
+@app.route("/member.html")
+def member():
+    return render_template("member.html")
+
+@app.route("/index.html")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
